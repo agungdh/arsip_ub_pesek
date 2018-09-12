eef11332f0dec0b1fe4bd6a015bc2d1b2ac73ca7
agungdh <agungdh@live.com>
2018-09-12 16:35:22 +0700

    update

--- application/views/cv_pt/index.php
+++ application/views/cv_pt/index.php
@@ -3,14 +3,14 @@
         <div class="card">
             <div class="header">
                 <h2>
-                    DATA LOKASI
+                    DATA CV/PT
                 </h2>
             </div>
             <div class="body">
                 <div class="button-demo">
-                  <a href="<?php echo base_url('lokasi/tambah'); ?>">
+                  <a href="<?php echo base_url('cv_pt/tambah'); ?>">
                     <button type="button" class="btn bg-blue waves-effect">
-                      <i class="material-icons">add</i>Tambah Lokasi
+                      <i class="material-icons">add</i>Tambah CV/PT
                     </button>
                   </a>
                 </div>
@@ -18,24 +18,30 @@
                     <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
                         <thead>
                             <tr>
-                                <th style="text-align: center;">Nama Lokasi</th>
+                                <th style="text-align: center;">Nama Perusahaan</th>
+                                <th style="text-align: center;">Nama Direktur</th>
+                                <th style="text-align: center;">Alamat</th>
+                                <th style="text-align: center;">No Telepon</th>
                                 <th style="text-align: center;">Proses</th>
                             </tr>
                         </thead>
                         <tbody>
                           <?php
-                          foreach ($this->db->get('lokasi')->result() as $item) {
+                          foreach ($this->db->get('cv_pt')->result() as $item) {
                             ?>
                             <tr>
-                              <td><?php echo $item->nama_lokasi; ?></td>
+                              <td><?php echo $item->nama_perusahaan; ?></td>
+                              <td><?php echo $item->nama_direktur; ?></td>
+                              <td><?php echo $item->alamat; ?></td>
+                              <td><?php echo $item->no_telepon; ?></td>
                                 <td style="text-align: center;">
-                                  <a href="<?php echo base_url('lokasi/ubah/' . $item->id_lokasi); ?>">
+                                  <a href="<?php echo base_url('cv_pt/ubah/' . $item->id_cv_pt); ?>">
                                     <button type="button" class="btn bg-blue waves-effect">
                                       <i class="material-icons">edit</i>
                                     </button>
                                   </a>
                                   <a href="javascript:void(0)">
-                                    <button type="button" class="btn bg-red waves-effect" onclick="hapus('<?php echo $item->id_lokasi; ?>')">
+                                    <button type="button" class="btn bg-red waves-effect" onclick="hapus('<?php echo $item->id_cv_pt; ?>')">
                                       <i class="material-icons">delete</i>
                                     </button>
                                   </a>
